c7c0767ce69a8482be872246d6199fa00b0d8d07
Preston <childrep@vt.edu>
2021-12-22 20:20:22 -0500

    Initial Arduino files for color-encoder

---
+++ calibration.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+uint16_t maxR, maxG, maxB;
+
+void setup(void) {
+  Serial.begin(9600);
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+    tcs.setInterrupt(1);
+  } else {
+    Serial.println("Not found");
+    while (1)
+      ;
+  }
+}
+
+void loop(void) {
+  uint16_t r, g, b, c;
+
+  tcs.getRawData(&r, &g, &b, &c);
+
+  if (r > maxR) {
+    maxR = r;
+  } else if (g > maxG) {
+    maxG = g;
+  } else if (b > maxB) {
+    maxB = b;
+  }
+
+  Serial.print("Max R: ", maxR);
+  Serial.print("Max G: ", maxG);
+  Serial.println("Max B: ", maxB);
+}
---
+++ encoder.ino
@@ -0,0 +1,47 @@
+#include <Wire.h>
+
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+uint16_t maxR = 300;
+uint16_t maxG = 200;
+uint16_t maxB = 100;
+
+uint16_t r, g, b, c;
+float scaledR, scaledG, scaledB;
+float minVal, maxVal, diff;
+float hue, sat, val;
+
+void setup(void) {
+  Serial.begin(9600);
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+    tcs.setInterrupt(1);
+  } else {
+    Serial.println("Not found");
+    while (1)
+      ;
+  }
+}
+
+void loop(void) {
+  tcs.getRawData(&r, &g, &b, &c);
+  scaledR = 1.0 * r / maxR;
+  scaledG = 1.0 * g / maxG;
+  scaledB = 1.0 * b / maxB;
+
+  maxVal = max(max(scaledR, scaledG), scaledB);
+  minVal = min(min(scaledR, scaledG), scaledB);
+  diff = maxVal - minVal;
+
+  if (maxVal == scaledR) {
+    hue = 60.0 * ((scaledG - scaledB) / diff % 6.0);
+  } else if (maxVal == scaledG) {
+    hue = 60.0 * ((scaledB - scaledR) / diff + 2.0);
+  } else if (maxVal == scaledB) {
+    hue = 60.0 * ((scaledR - scaledG) / diff + 4.0);
+  }
+
+  Serial.print("Hue angle: ", hue);
+}
